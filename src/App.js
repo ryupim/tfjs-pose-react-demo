9db1fbbac6fe245311d098b9349ce4a55f25e69d
ryupim <shingai92@gmail.com>
2022-09-19 20:59:58 +0900

    Change the mediapipe version & format complited

--- src/App.js
+++ src/App.js
@@ -9,91 +9,91 @@ import Webcam from "react-webcam";
 import { drawKeypoints, drawSkeleton } from "./utilities";
 
 function App() {
-  const webcamRef = useRef(null);
-  const canvasRef = useRef(null);
+    const webcamRef = useRef(null);
+    const canvasRef = useRef(null);
 
-  // Load posenet
-  const runPoseEstimation = async () => {
-    // const net = await posenet.load({
-    //   inputResolution: { width: 640, height: 480 },
-    //   scale: 0.5,
-    // });
+    // Load posenet
+    const runPoseEstimation = async () => {
+        // const net = await posenet.load({
+        //   inputResolution: { width: 640, height: 480 },
+        //   scale: 0.5,
+        // });
 
-    const model = posedetection.SupportedModels.MoveNet;
-    const net = await posedetection.createDetector(model);
-    setInterval(() => {
-      detect(net);
-    }, 100);
-  };
+        const model = posedetection.SupportedModels.MoveNet;
+        const net = await posedetection.createDetector(model);
+        setInterval(() => {
+            detect(net);
+        }, 100);
+    };
 
-  const detect = async (net) => {
-    if (
-      typeof webcamRef.current !== "undefined" &&
-      webcamRef.current !== null &&
-      webcamRef.current.video.readyState === 4
-    ) {
-      // Get Video Properties
-      const video = webcamRef.current.video;
-      const videoWidth = webcamRef.current.video.videoWidth;
-      const videoHeight = webcamRef.current.video.videoHeight;
+    const detect = async (net) => {
+        if (
+            typeof webcamRef.current !== "undefined" &&
+            webcamRef.current !== null &&
+            webcamRef.current.video.readyState === 4
+        ) {
+            // Get Video Properties
+            const video = webcamRef.current.video;
+            const videoWidth = webcamRef.current.video.videoWidth;
+            const videoHeight = webcamRef.current.video.videoHeight;
 
-      // Set vidoe width
-      webcamRef.current.video.width = videoWidth;
-      webcamRef.current.video.height = videoHeight;
+            // Set vidoe width
+            webcamRef.current.video.width = videoWidth;
+            webcamRef.current.video.height = videoHeight;
 
-      // Make Detections
-      const pose = await net.estimatePoses(video);
-      console.log(pose);
+            // Make Detections
+            const pose = await net.estimatePoses(video);
+            console.log(pose);
 
-      drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
-    }
-  };
+            drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
+        }
+    };
 
-  const drawCanvas = (pose, video, videoWidth, vidoeHeight, canvas) => {
-    const ctx = canvas.current.getContext("2d");
-    canvas.current.width = videoWidth;
-    canvas.current.height = vidoeHeight;
+    const drawCanvas = (pose, video, videoWidth, vidoeHeight, canvas) => {
+        const ctx = canvas.current.getContext("2d");
+        canvas.current.width = videoWidth;
+        canvas.current.height = vidoeHeight;
 
-    drawKeypoints(pose[0]["keypoints"], 0.5, ctx);
-    drawSkeleton(pose[0]["keypoints"], 0.5, ctx);
-  };
-  runPoseEstimation();
+        drawKeypoints(pose[0]["keypoints"], 0.5, ctx);
+        drawSkeleton(pose[0]["keypoints"], 0.5, ctx);
+    };
+    runPoseEstimation();
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Webcam
-          ref={webcamRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 9,
-            width: 640,
-            height: 480,
-          }}
-        />
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Webcam
+                    ref={webcamRef}
+                    style={{
+                        position: "absolute",
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                        left: 0,
+                        right: 0,
+                        textAlign: "center",
+                        zindex: 9,
+                        width: 640,
+                        height: 480,
+                    }}
+                />
 
-        <canvas
-          ref={canvasRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 9,
-            width: 640,
-            height: 480,
-          }}
-        />
-      </header>
-    </div>
-  );
+                <canvas
+                    ref={canvasRef}
+                    style={{
+                        position: "absolute",
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                        left: 0,
+                        right: 0,
+                        textAlign: "center",
+                        zindex: 9,
+                        width: 640,
+                        height: 480,
+                    }}
+                />
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/utilities.js
+++ src/utilities.js
@@ -27,117 +27,117 @@ const tryResNetButtonTextCss = "width:100%;text-decoration:underline;";
 const tryResNetButtonBackgroundCss = "background:#e61d5f;";
 
 function isAndroid() {
-  return /Android/i.test(navigator.userAgent);
+    return /Android/i.test(navigator.userAgent);
 }
 
 function isiOS() {
-  return /iPhone|iPad|iPod/i.test(navigator.userAgent);
+    return /iPhone|iPad|iPod/i.test(navigator.userAgent);
 }
 
 export function isMobile() {
-  return isAndroid() || isiOS();
+    return isAndroid() || isiOS();
 }
 
 function setDatGuiPropertyCss(propertyText, liCssString, spanCssString = "") {
-  var spans = document.getElementsByClassName("property-name");
-  for (var i = 0; i < spans.length; i++) {
-    var text = spans[i].textContent || spans[i].innerText;
-    if (text == propertyText) {
-      spans[i].parentNode.parentNode.style = liCssString;
-      if (spanCssString !== "") {
-        spans[i].style = spanCssString;
-      }
+    var spans = document.getElementsByClassName("property-name");
+    for (var i = 0; i < spans.length; i++) {
+        var text = spans[i].textContent || spans[i].innerText;
+        if (text == propertyText) {
+            spans[i].parentNode.parentNode.style = liCssString;
+            if (spanCssString !== "") {
+                spans[i].style = spanCssString;
+            }
+        }
     }
-  }
 }
 
 export function updateTryResNetButtonDatGuiCss() {
-  setDatGuiPropertyCss(
-    tryResNetButtonText,
-    tryResNetButtonBackgroundCss,
-    tryResNetButtonTextCss
-  );
+    setDatGuiPropertyCss(
+        tryResNetButtonText,
+        tryResNetButtonBackgroundCss,
+        tryResNetButtonTextCss
+    );
 }
 
 /**
  * Toggles between the loading UI and the main canvas UI.
  */
 export function toggleLoadingUI(
-  showLoadingUI,
-  loadingDivId = "loading",
-  mainDivId = "main"
+    showLoadingUI,
+    loadingDivId = "loading",
+    mainDivId = "main"
 ) {
-  if (showLoadingUI) {
-    document.getElementById(loadingDivId).style.display = "block";
-    document.getElementById(mainDivId).style.display = "none";
-  } else {
-    document.getElementById(loadingDivId).style.display = "none";
-    document.getElementById(mainDivId).style.display = "block";
-  }
+    if (showLoadingUI) {
+        document.getElementById(loadingDivId).style.display = "block";
+        document.getElementById(mainDivId).style.display = "none";
+    } else {
+        document.getElementById(loadingDivId).style.display = "none";
+        document.getElementById(mainDivId).style.display = "block";
+    }
 }
 
 function toTuple({ y, x }) {
-  return [y, x];
+    return [y, x];
 }
 
 function toTuplePosition(keypoint) {
-  const { y, x } = keypoint;
-  return [y, x];
+    const { y, x } = keypoint;
+    return [y, x];
 }
 
 export function drawPoint(ctx, y, x, r, color) {
-  ctx.beginPath();
-  ctx.arc(x, y, r, 0, 2 * Math.PI);
-  ctx.fillStyle = color;
-  ctx.fill();
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, 2 * Math.PI);
+    ctx.fillStyle = color;
+    ctx.fill();
 }
 
 /**
  * Draws a line on a canvas, i.e. a joint
  */
 export function drawSegment([ay, ax], [by, bx], color, scale, ctx) {
-  ctx.beginPath();
-  ctx.moveTo(ax * scale, ay * scale);
-  ctx.lineTo(bx * scale, by * scale);
-  ctx.lineWidth = lineWidth;
-  ctx.strokeStyle = color;
-  ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(ax * scale, ay * scale);
+    ctx.lineTo(bx * scale, by * scale);
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = color;
+    ctx.stroke();
 }
 
 /**
  * Draws a pose skeleton by looking up all adjacent keypoints/joints
  */
 export function drawSkeleton(keypoints, minConfidence, ctx, scale = 1) {
-  const adjacentKeyPoints = posenet.getAdjacentKeyPoints(
-    keypoints,
-    minConfidence
-  );
-
-  adjacentKeyPoints.forEach((keypoints) => {
-    drawSegment(
-      toTuplePosition(keypoints[0]),
-      toTuplePosition(keypoints[1]),
-      color,
-      scale,
-      ctx
+    const adjacentKeyPoints = posenet.getAdjacentKeyPoints(
+        keypoints,
+        minConfidence
     );
-  });
+
+    adjacentKeyPoints.forEach((keypoints) => {
+        drawSegment(
+            toTuplePosition(keypoints[0]),
+            toTuplePosition(keypoints[1]),
+            color,
+            scale,
+            ctx
+        );
+    });
 }
 
 /**
  * Draw pose keypoints onto a canvas
  */
 export function drawKeypoints(keypoints, minConfidence, ctx, scale = 1) {
-  for (let i = 0; i < keypoints.length; i++) {
-    const keypoint = keypoints[i];
+    for (let i = 0; i < keypoints.length; i++) {
+        const keypoint = keypoints[i];
 
-    if (keypoint.score < minConfidence) {
-      continue;
+        if (keypoint.score < minConfidence) {
+            continue;
+        }
+
+        const { y, x } = keypoint;
+        drawPoint(ctx, y * scale, x * scale, 3, color);
     }
-
-    const { y, x } = keypoint;
-    drawPoint(ctx, y * scale, x * scale, 3, color);
-  }
 }
 
 /**
@@ -146,50 +146,50 @@ export function drawKeypoints(keypoints, minConfidence, ctx, scale = 1) {
  * ankles
  */
 export function drawBoundingBox(keypoints, ctx) {
-  const boundingBox = posenet.getBoundingBox(keypoints);
+    const boundingBox = posenet.getBoundingBox(keypoints);
 
-  ctx.rect(
-    boundingBox.minX,
-    boundingBox.minY,
-    boundingBox.maxX - boundingBox.minX,
-    boundingBox.maxY - boundingBox.minY
-  );
+    ctx.rect(
+        boundingBox.minX,
+        boundingBox.minY,
+        boundingBox.maxX - boundingBox.minX,
+        boundingBox.maxY - boundingBox.minY
+    );
 
-  ctx.strokeStyle = boundingBoxColor;
-  ctx.stroke();
+    ctx.strokeStyle = boundingBoxColor;
+    ctx.stroke();
 }
 
 /**
  * Converts an arary of pixel data into an ImageData object
  */
 export async function renderToCanvas(a, ctx) {
-  const [height, width] = a.shape;
-  const imageData = new ImageData(width, height);
+    const [height, width] = a.shape;
+    const imageData = new ImageData(width, height);
 
-  const data = await a.data();
+    const data = await a.data();
 
-  for (let i = 0; i < height * width; ++i) {
-    const j = i * 4;
-    const k = i * 3;
+    for (let i = 0; i < height * width; ++i) {
+        const j = i * 4;
+        const k = i * 3;
 
-    imageData.data[j + 0] = data[k + 0];
-    imageData.data[j + 1] = data[k + 1];
-    imageData.data[j + 2] = data[k + 2];
-    imageData.data[j + 3] = 255;
-  }
+        imageData.data[j + 0] = data[k + 0];
+        imageData.data[j + 1] = data[k + 1];
+        imageData.data[j + 2] = data[k + 2];
+        imageData.data[j + 3] = 255;
+    }
 
-  ctx.putImageData(imageData, 0, 0);
+    ctx.putImageData(imageData, 0, 0);
 }
 
 /**
  * Draw an image on a canvas
  */
 export function renderImageToCanvas(image, size, canvas) {
-  canvas.width = size[0];
-  canvas.height = size[1];
-  const ctx = canvas.getContext("2d");
+    canvas.width = size[0];
+    canvas.height = size[1];
+    const ctx = canvas.getContext("2d");
 
-  ctx.drawImage(image, 0, 0);
+    ctx.drawImage(image, 0, 0);
 }
 
 /**
@@ -198,11 +198,11 @@ export function renderImageToCanvas(image, size, canvas) {
  * https://medium.com/tensorflow/real-time-human-pose-estimation-in-the-browser-with-tensorflow-js-7dd0bc881cd5
  */
 export function drawHeatMapValues(heatMapValues, outputStride, canvas) {
-  const ctx = canvas.getContext("2d");
-  const radius = 5;
-  const scaledValues = heatMapValues.mul(tf.scalar(outputStride, "int32"));
+    const ctx = canvas.getContext("2d");
+    const radius = 5;
+    const scaledValues = heatMapValues.mul(tf.scalar(outputStride, "int32"));
 
-  drawPoints(ctx, scaledValues, radius, color);
+    drawPoints(ctx, scaledValues, radius, color);
 }
 
 /**
@@ -210,19 +210,19 @@ export function drawHeatMapValues(heatMapValues, outputStride, canvas) {
  * the canvas
  */
 function drawPoints(ctx, points, radius, color) {
-  const data = points.buffer().values;
+    const data = points.buffer().values;
 
-  for (let i = 0; i < data.length; i += 2) {
-    const pointY = data[i];
-    const pointX = data[i + 1];
+    for (let i = 0; i < data.length; i += 2) {
+        const pointY = data[i];
+        const pointX = data[i + 1];
 
-    if (pointX !== 0 && pointY !== 0) {
-      ctx.beginPath();
-      ctx.arc(pointX, pointY, radius, 0, 2 * Math.PI);
-      ctx.fillStyle = color;
-      ctx.fill();
+        if (pointX !== 0 && pointY !== 0) {
+            ctx.beginPath();
+            ctx.arc(pointX, pointY, radius, 0, 2 * Math.PI);
+            ctx.fillStyle = color;
+            ctx.fill();
+        }
     }
-  }
 }
 
 /**
